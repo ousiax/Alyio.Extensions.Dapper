73558528f073440ce229da03f4f9142e8562dba2
XU <qqbuby@gmail.com>
2022-07-21 11:30:17 +0800

    Add Dapper.Extensions.DataAccess.MySql

---
+++ src/Dapper.Extensions.DataAccess.MySql/MySqlConnectionFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+
+using Microsoft.Extensions.Options;
+
+using MySqlConnector;
+
+namespace Dapper.Extensions.DataAccess
+{
+    internal sealed class MySqlConnectionFactory : IConnectionFactory
+    {
+        private readonly IOptions<MySqlConnectionOptions> _options;
+
+        public MySqlConnectionFactory(IOptions<MySqlConnectionOptions> options)
+        {
+            _options = options;
+        }
+
+        public async Task<IDbConnection> OpenAsync()
+        {
+            var conn = new MySqlConnection(_options.Value.Master);
+            await conn.OpenAsync().ConfigureAwait(false);
+            return conn;
+        }
+
+        public async Task<IDbConnection> OpenReadOnlyAsync()
+        {
+            var conn = new MySqlConnection(_options.Value.Slave?[0]);
+            await conn.OpenAsync().ConfigureAwait(false);
+            return conn;
+        }
+    }
+}
---
+++ src/Dapper.Extensions.DataAccess.MySql/MySqlConnectionOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace Dapper.Extensions.DataAccess
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MySqlConnectionOptions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public string? Master { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public IList<string>? Slave { get; set; }
+    }
+}
