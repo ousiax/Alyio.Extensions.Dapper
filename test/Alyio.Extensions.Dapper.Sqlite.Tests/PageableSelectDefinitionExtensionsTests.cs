33795a2ca1da88ec85a913ff51f2925046f85994
XU <qqbuby@gmail.com>
2022-07-26 10:34:14 +0800

    Add PageableSelectDefinitionExtensionsTests.cs

---
+++ test/Alyio.Extensions.Dapper.Sqlite.Tests/PageableSelectDefinitionExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Alyio.Extensions.Dapper.Sqlite.Tests
+{
+    public class PageableSelectDefinitionExtensionsTests
+    {
+        [Fact]
+        public Task TestThrowArgumentExceptionAsync()
+        {
+            var exception = Assert.Throws<ArgumentException>(() =>
+            {
+                return new SelectDefinition().AsPageable(0, -5);
+            });
+
+            Assert.Equal("The pageSize must be greater than zero.", exception.Message);
+
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public Task TestThrowInvalidCastExceptionAsync()
+        {
+            var exception = Assert.Throws<InvalidCastException>(() =>
+            {
+                return new SelectDefinition { CommandType = System.Data.CommandType.StoredProcedure }.AsPageable(0, 5);
+            });
+
+            Assert.Contains("must be CommandType.Text", exception.Message);
+
+            return Task.CompletedTask;
+        }
+    }
+}
