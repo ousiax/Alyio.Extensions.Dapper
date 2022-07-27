7bc8d939cf2f7a5b897f72e0554e0e884eb669a3
XU <qqbuby@gmail.com>
2022-07-27 12:06:57 +0800

    Add SqlDefinitionTest.cs

---
+++ test/Alyio.Extensions.Dapper.Sqlite.Tests/SqlDefinitionTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+
+namespace Alyio.Extensions.Dapper.Sqlite.Tests
+{
+    public class SqlDefinitionTest
+    {
+        [Theory]
+        [InlineData("selectbyid", 60, "select * from sometable where id = @id", CommandType.Text)]
+        [InlineData("selectbyname", 120, "select * from sometable where name = @name order by name desc", CommandType.Text)]
+        public void TestToString(string id, int timeout, string text, CommandType type)
+        {
+            // $"Id: {Id}, CommandTimeout: {CommandTimeout}, CommandText: {CommandText}, CommandType: {CommandType}"
+            var selDef = new SelectDefinition
+            {
+                Id = id,
+                CommandTimeout = timeout,
+                CommandText = text,
+                CommandType = type,
+            };
+
+            var exptected = $"Id: {id}, CommandTimeout: {timeout}, CommandText: {text}, CommandType: {type}";
+
+            Assert.Equal(exptected, selDef.ToString());
+        }
+    }
+}
