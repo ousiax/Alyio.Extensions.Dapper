045deff643072b4ec980caaf04f22c9a25fe23df
Jon.X <ousiax@hotmail.com>
2023-11-13 12:05:08 +0800

    Use coalesce expression for null check

--- src/Alyio.Extensions.Dapper/ConfigurationProvider.cs
+++ src/Alyio.Extensions.Dapper/ConfigurationProvider.cs
@@ -16,11 +16,7 @@ namespace Alyio.Extensions.Dapper
             {
                 var type = Type.GetType(mapperDefinition.Type, true);
                 var name = mapperDefinition.Name ?? $"{type.Namespace}.{type.Name}.xml";
-                using var resource = type.Assembly.GetManifestResourceStream(name);
-                if (resource == null)
-                {
-                    throw new ArgumentException($"Couldn't get manifest resource of type `{mapperDefinition.Type}` with name `{name}`.");
-                }
+                using var resource = type.Assembly.GetManifestResourceStream(name) ?? throw new ArgumentException($"Couldn't get manifest resource of type `{mapperDefinition.Type}` with name `{name}`.");
                 var xmlMapper = new XmlSerializer(typeof(Mapper));
                 var mapper = (Mapper)xmlMapper.Deserialize(resource);
                 Mappers.Add(type, mapper);
